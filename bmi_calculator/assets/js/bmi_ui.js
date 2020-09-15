70a053a0acb63425f73acf3608fce02c11243db8
unknown <Administrator@STO-102PC02.TRAINING.local>
2020-09-15 11:11:47 +0200

    Finished bmi excercise

--- bmi_calculator/assets/js/bmi_ui.js
+++ bmi_calculator/assets/js/bmi_ui.js
@@ -1,13 +1,32 @@
-const button = document.getElementById('calculate')
-const weightInput = document.getElementById('weight')
-const heightInput = document.getElementById('height')
-const resultsNode = document.getElementById('results')
+const button = document.getElementById("calculate");
+const weightInput = document.getElementById("weight");
+const heightInput = document.getElementById("height");
+const resultsNode = document.getElementById("results");
+const selectorNode = document.getElementById("method");
+let method = "metric";
 
+const calculateBmi = () =>{
+    let bmi;
+    if (method == "metric") {
+      bmi = bmiClaculator.calculateMetric(weightInput.value, heightInput.value);
+    } else {
+      bmi = bmiClaculator.calculateImperial(weightInput.value, heightInput.value);
+    }
+    resultsNode.innerHTML = `<h2>Your BMI is ${bmi.value.toFixed(2)}, 
+      you are considered ${bmi.display.message}!</h2>`;
+    resultsNode.style.color = bmi.display.color;
+}
+button.addEventListener("click", () => {
+    calculateBmi()
+});
 
-button.addEventListener('click', function(){
-    let bmi = bmiClaculator.calculateMetric(weightInput.value, heightInput.value)
-    resultsNode.innerHTML = `<h2>Your BMI value is ${bmi.value.toFixed(2)}, 
-    you are considered ${bmi.display.message}!</h2>`
-    resultsNode.style.color = bmi.display.color
-
-})
+selectorNode.addEventListener("change", () => {
+  method = selectorNode.options[selectorNode.selectedIndex].value;
+  if (method === "imperial") {
+    weightInput.placeholder = weightInput.placeholder.replace("kg", "pounds");
+    heightInput.placeholder = heightInput.placeholder.replace("cm", "inches");
+  } else {
+    weightInput.placeholder = weightInput.placeholder.replace("pounds", "kg");
+    heightInput.placeholder = heightInput.placeholder.replace("inches", "cm");
+  }
+});
